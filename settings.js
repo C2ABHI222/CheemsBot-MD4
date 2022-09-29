784f468befe4e02cca6339a8b6a6a07e4b63d76b
C2ABHI222 <112750752+C2ABHI222@users.noreply.github.com>
2022-09-29 09:47:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
+global.vcardowner = ['919745580745'] //ur owner number
 global.ownername = "🦄Dream Guy Xeon" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['919745580745']
+global.ownertag = '919745580745' //ur tag number
+global.botname = 'ꪶ⛧͢ɪͥᴛͭsᷤ ᴍͫᴇͤ￫ 𝙲͢𝟸᮫ ͎ᴀⷶʙⷡ᷍ʜⷩɪ̶ ͥ' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "🙂" //ur menu react emoji
+global.themeemoji = "💕" //ur theme emoji
+global.packname = "ꪶ⛧͢ɪͥᴛͭsᷤ ᴍͫᴇͤ￫ 𝙲͢𝟸᮫ ͎ᴀⷶʙⷡ᷍ʜⷩɪ̶ ͥ" //ur sticker watermark packname
+global.author = "ᬤ൬ࣶྂ ➝➛ സ്വ᳒͢ന്ത◕ സ᭄͢ꫧ മ͎ഹꪆල͢ᘡക്ഷᤨ͟മ᭄" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['919745580745'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
